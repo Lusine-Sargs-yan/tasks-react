c11a0aff0c629103754412eb4d0fd5baf986a227
Lusine Sargsyan <sargsyanlq@gmail.com>
2021-04-29 11:38:40 +0400

    add image description

--- src/components/MuseumImage/MuseumImage.js
+++ src/components/MuseumImage/MuseumImage.js
@@ -2,16 +2,17 @@ import { render } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 
 const BASE_URL = "https://harvardartmuseums.org/browse";
+const BASE_URL2 = "https://harvardartmuseums.org/browse?load_amount=30&offset=12";
 
 function MuseumImage() {
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
-    fetch(BASE_URL)
+    fetch(BASE_URL2)
       .then((response) => response.json())
       .then((data) => {
         setRecords(data.records);
-        //console.log(data.info);
+        console.log(data.info);
       });
   }, []);
 
@@ -20,17 +21,22 @@ function MuseumImage() {
   return (
     <div>
       <h2>Harvard art museums</h2>
-      {records.map(({ primaryimageurl, title, division }, index) => {
-        // let image = images.map(({baseimageurl}) => baseimageurl);
+      {records.map(({ primaryimageurl, title, division, culture,copyright, id }, index) => {
+    
         return (
-          <div key={index}>
+          <div key={id}>
             <img width="90" src={primaryimageurl} />
-            <h4>{title}</h4>
+            <div>M{id}</div>
+            <h5>{title}</h5>
+            <div>Culture {culture}</div>
+            <div>{copyright}</div>
             <div>{division}</div>
+            <div>{index + 1}</div>
             <br />
           </div>
         );
       })}
+      <div>{records.length}</div>
     </div>
   );
 }
